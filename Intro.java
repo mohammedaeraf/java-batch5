ab315cc2a5ce355b108005ddbe3fcb402600034b
Mohammed Aeraf <aeraf@outlook.com>
2025-09-13 10:25:51 +0530

    first commit

---
+++ Intro.java
@@ -0,0 +1,11 @@
+class Intro {
+    public static void main(String[] args) {
+        int x = 10; // x is a variable (container)
+        float pi = 3.142f;
+
+        int y = x + 5;
+        System.out.println("x ki value hai " + x);
+        System.out.println("y ki value hai " + y);
+
+    }
+}
---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table {
+
+    public static void main(String[] args) {
+        int n = 2;
+        for (int i = 1; i <= 10; i++) {
+            int result = n * i; 
+            System.out.println(n + " x " + i + " = " + result);
+        }
+    }
+
+}
